965e600ea4437f3f4c27f4b3f6992845a3227a37
alexyekymov <alexyekymov@gmail.com>
2024-01-06 23:34:44 +0200

    fix: fix deployment & cleanup

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -16,11 +15,8 @@ export default defineConfig({
     }
   },
   server: {
-    watch: {
-      usePolling: true
-    },
     host: true,
-    strictPort: true,
     port: 5173
-  }
+  },
+  base: '/ussd'
 })
